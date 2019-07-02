17aa522c589d283ff6891291987a514f4046666d
Jason Pitman <jason@ff4500.com>
2019-07-02 17:28:44 -0500

    definitely NOT porn thumbnail script

---
+++ images/make_250x250_thumbnails/go.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+input_dir="$HOME/Desktop/Definitely_NOT_Porn"
+exts="jpg,jpeg,JPG,png,gif"
+output_dir="$HOME/Desktop/notporn_thumbnails"
+
+if [ ! -d "$output_dir" ]; then
+  mkdir -p "$output_dir"
+fi
+
+for notpornimages in $input_dir/*.{$exts} ; do
+
+  convert "$notpornimages" \
+    -thumbnail 250x250^ \
+    -gravity center \
+    -extent 250x250 \
+    -set filename:fname '%t_thumb' \
+    "$output_dir"/'%[filename:fname].jpg'
+
+done
